7929c20fd5d63c9cfe3e4d9a6d7d8e4e69188395
AmrAbouElkair <amrmahmooud20022002@gmail.com>
2024-02-20 23:51:32 +0200

    packages and project initialization

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+// import "mdb-react-ui-kit/dist/css/mdb.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+// fontawsome
+import "../node_modules/@fortawesome/fontawesome-free/css/all.min.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hello</h1>
+    </>
+  );
+}
+
+export default App;
