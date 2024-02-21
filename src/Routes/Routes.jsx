ecac251f1741a9a66feb45087fce02ed949ed003
AmrAbouElkair <amrmahmooud20022002@gmail.com>
2024-02-21 13:51:44 +0200

    Create Checkout page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -9,7 +9,7 @@ const Products = lazy(() => import("../views/Products/Products"));
 const Login = lazy(() => import("../views/Login/Login"));
 const Register = lazy(() => import("../views/Register/Register"));
 const MainCart = lazy(() => import("../views/MainCart/MainCart"));
-const Checkout = lazy(() => import("../views/Checkout"));
+const Checkout = lazy(() => import("../views/Checkout/Checkout"));
 const NewProduct = lazy(() => import("../views/NewProduct"));
 const NotFound = lazy(() => import("../views/NotFound"));
 
